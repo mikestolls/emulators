2440c9999b8342ab5a41aeffd4118d553aeea24e
mikestolls <mike.stolls@gmail.com>
2020-03-26 19:04:58 -0400

    Update run_unittest.bat

--- make/run_unittest.bat
+++ make/run_unittest.bat
@@ -2,7 +2,7 @@
 
 pushd %~dp0
 
-"..\buil\Release\emulators\emulators.exe -unittest ..\..\..\data\gameboy\unit_test.json"
+"..\build\Release\emulators\emulators.exe -unittest ..\data\gameboy\unit_test.json"
 
 popd
 
