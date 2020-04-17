6f29e84e1fd55a05e8043830e90f8273f3463c95
mikestolls <mike.stolls@gmail.com>
2020-04-17 13:15:25 -0400

    adding pip calls to run test

--- make/run_unittest.bat
+++ make/run_unittest.bat
@@ -5,6 +5,7 @@ pushd %~dp0
 rmdir /s /q ..\_test_results
 mkdir ..\_test_results
 
+pip install argparse junit_xml --user
 python run_unittest.py --emulator ..\_build\Release\emulators\emulators.exe --unit_test_filename ..\data\gameboy\unit_test.json --results_dir ..\_test_results
 
 popd
