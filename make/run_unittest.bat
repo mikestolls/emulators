2176d3b33c80338dcc08522f3bc27c81c5c0484c
mikestolls <mike.stolls@gmail.com>
2020-04-17 11:50:18 -0400

    trying python3 with circleci

--- make/run_unittest.bat
+++ make/run_unittest.bat
@@ -5,6 +5,6 @@ pushd %~dp0
 rmdir /s /q ..\_test_results
 mkdir ..\_test_results
 
-python run_unittest.py --emulator ..\_build\Release\emulators\emulators.exe --unit_test_filename ..\data\gameboy\unit_test.json --results_dir ..\_test_results
+python3 run_unittest.py --emulator ..\_build\Release\emulators\emulators.exe --unit_test_filename ..\data\gameboy\unit_test.json --results_dir ..\_test_results
 
 popd
