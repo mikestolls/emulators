61e7bd0cc40922a890d237c986a70f5ed6383481
mikestolls <mike.stolls@gmail.com>
2025-07-07 19:54:58 -0400

    adding common emulator window

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,16 @@
 // main.c : Defines the entry point for the console application.
 //
 
-#include "chip8\chip8.h"
-#include "gameboy\gameboy.h"
+#include "common/main_window.h"
+#include "chip8/chip8.h"
+#include "gameboy/gameboy.h"
 
 int main(int argc, const char* argv[])
 {
     // main entry point for all emulators. for now just run emulator, eventually could add more global function to reset all emulators, control mapping, etc.
     //int ret = chip8::run_emulator(argc, argv);
-    int ret = gameboy::run_emulator(argc, argv);
+    //int ret = gameboy::run_emulator(argc, argv);
+	int ret = common::emulator_window();
 
     return ret;
 }
