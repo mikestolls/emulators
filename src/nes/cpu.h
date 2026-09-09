00fb70e90efbc65c2df6635ce68ebbea9f2c3c4f
mikestolls <mike.stolls@gmail.com>
2026-09-09 10:25:49 -0400

    stubbing out some of the cpu opcode decoding. stubbing out some alu funcs and modify funds

--- src/nes/cpu.h
+++ src/nes/cpu.h
@@ -13,13 +13,27 @@ namespace nes
 		enum MICRO_OP_TYPE
 		{
 			NOP,
-			FETCH_OP
+			FETCH_OP,
+			READ_BUS_EXECUTE_ALU,
+			READ_MODIFY_WRITE
 		};
 
 		struct MicroOp
 		{			
 			MICRO_OP_TYPE micro_op_type;
+			void(*alu_funct)(u8);
+			u8(*mod_funct)(u8);
 		};
+
+		struct Registers
+		{
+			u8 a;
+			u8 x;
+			u8 y;
+			u16 pc;
+			u8 sp;
+			u8 p;
+		} R;
 
 		bool running = true;
 		u16 pc = 0x0;
@@ -57,6 +71,14 @@ namespace nes
 
 			micro_op_queue.clear();
 
+			// initial values
+			R.a = 0x0;
+			R.x = 0x0;
+			R.y = 0x0;
+			R.pc = 0x0;
+			R.sp = 0x0;
+			R.p = 0x0;
+
 			return 0;
 		}
 
@@ -73,6 +95,181 @@ namespace nes
 
 			return 0;
 		}
+
+		// temp placing here but move up later
+		// ALU function pointers
+		inline void alu_ora(u8 data)
+		{
+			// bitwise or
+		}
+
+		inline void alu_and(u8 data)
+		{
+			// bitwise and
+		}
+
+		inline void alu_eor(u8 data)
+		{
+			// bitwise xor
+		}
+
+		inline void alu_adc(u8 data)
+		{
+			// add with carry
+		}
+
+		inline void alu_lda(u8 data)
+		{
+			// ld accumulator
+		}
+
+		inline void alu_cmp(u8 data)
+		{
+			// compare
+		}
+
+		inline void alu_sbc(u8 data)
+		{
+			// subtract with carry
+			alu_adc(data); // invert bits of data and pass to add
+		}
+
+		inline void alu_bit(u8 data)
+		{
+			// bit test
+		}
+
+		inline void alu_ldy(u8 data)
+		{
+			// load y reg
+		}
+
+		inline void alu_cpy(u8 data)
+		{
+			// compare y reg
+		}
+
+		inline void alu_cpx(u8 data)
+		{
+			// compare x reg
+		}
+
+		void(*alu_function_group_0[])(u8) = { nullptr, nullptr, nullptr, nullptr, alu_bit, alu_ldy, alu_cpy, alu_cpx };
+		void(*alu_function_group_1[])(u8) = { alu_ora, alu_and, alu_eor, alu_adc, nullptr, alu_lda, alu_cmp, alu_sbc };
+
+		inline u8 mod_asl(u8 data)
+		{
+			// shift left
+			return 0;
+		}
+
+		inline u8 mod_rol(u8 data)
+		{
+			// rotate left
+			return 0;
+		}
+
+		inline u8 mod_lsr(u8 data)
+		{
+			// logical shift right
+			return 0;
+		}
+
+		inline u8 mod_ror(u8 data)
+		{
+			// rotate right
+			return 0;
+		}
+
+		inline u8 mod_dec(u8 data)
+		{
+			// dec mem
+			return 0;
+		}
+
+		inline u8 mod_inc(u8 data)
+		{
+			// inc mem
+			return 0;
+		}
+
+		u8(*mod_functions_group_2[])(u8) = { mod_asl, mod_rol, mod_lsr, mod_ror, nullptr, nullptr, mod_dec, mod_inc };
+
+		int decode_opcode(u8 opcode)
+		{
+			// decode by spliting opcode into aaabbbcc format
+			u8 aaa = (opcode >> 5) & 0x7;
+			u8 bbb = (opcode >> 2) & 0x7;
+			u8 cc = (opcode & 0x3);
+
+			switch (cc)
+			{
+			case 0x0: // op group for control and misc alu ops
+			{
+				if (aaa == 0x4)
+				{
+					// STY
+				}
+				else if (aaa == 0x2)
+				{
+					// JMP absolute
+				}
+				else if (aaa == 0x3)
+				{
+					// JMP indirect
+				}
+				else
+				{
+					// other alu ops (bit, ldy, cpy, cpx)
+					MicroOp read_bus_exec;
+					read_bus_exec.micro_op_type = READ_BUS_EXECUTE_ALU;
+					read_bus_exec.alu_funct = alu_function_group_0[aaa];
+				}
+				break;
+			}
+			case 0x1: // op group for alu, load and store
+			{
+				if (aaa == 0x4)
+				{
+					// STA
+				}
+				else
+				{
+					// other ALU ops (ora, and, eor, adc, lda, cmp, sbc)
+					MicroOp read_bus_exec;
+					read_bus_exec.micro_op_type = READ_BUS_EXECUTE_ALU;
+					read_bus_exec.alu_funct = alu_function_group_1[aaa];
+				}
+				break;
+			}
+			case 0x2: // op group for bitwise ops and x registry
+			{
+				if (aaa == 0x4) 
+				{
+					// STX
+				}
+				else if (aaa == 0x5) 
+				{
+					// LDX
+				}
+				else
+				{
+					// other mod ops (asl, rol, lsr, ror, dec, inc)
+					MicroOp read_modify_write;
+					read_modify_write.micro_op_type = READ_MODIFY_WRITE;
+					read_modify_write.mod_funct = mod_functions_group_2[aaa];
+				}
+				break;
+			}
+			case 0x3: // op group 3 - undocumnted illegal opcodes but some roms may use them
+			{
+				assert(false);
+				break;
+			}
+			}
+
+			return 0;
+		}
 				
 		int execute_micro_op(MicroOp& op)
 		{
@@ -86,6 +283,8 @@ namespace nes
 			case MICRO_OP_TYPE::FETCH_OP:
 			{
 				u8 opcode = readpc_u8();
+
+				decode_opcode(opcode);
 
 				// set this as we have started a new opcode
 				is_opcode_complete = false;
